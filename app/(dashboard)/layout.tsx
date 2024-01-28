40d683a602140a690c2b66a2635322c641e68ed1
mcakeyerima <mcakyerima@gmail.com>
2024-01-28 23:15:29 +0100

    sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+const DashboardLayout = () => {
+    return ( 
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-56">
+                sidebar
+            </div>
+        </div>
+     );
+}
+ 
+export default DashboardLayout;
