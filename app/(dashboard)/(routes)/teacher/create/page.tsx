dcb0bd39818e920f05c88be5334ef2733a34e7b3
mcakeyerima <mcakyerima@gmail.com>
2024-02-04 23:51:51 +0100

    updated courses page

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,6 +1,36 @@
 "use client";
 
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+
+import {
+    Form,
+    FormDescription,
+    FormControl,
+    FormField,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+
+
+// create a form schema
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "Title is required",
+    }),
+});
+
 const CreatePage = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: ""
+        },
+    });
+
     return ( 
         <div>
             Create Page
