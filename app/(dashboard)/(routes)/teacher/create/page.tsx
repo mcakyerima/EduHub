8479d7536da7ade24e079f37a845b07ce125612c
mcakeyerima <mcakyerima@gmail.com>
2024-02-06 23:26:37 +0100

    update

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 import {
     Form,
@@ -13,7 +14,10 @@ import {
     FormField,
     FormLabel,
     FormMessage,
+    FormItem,
 } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 
 // create a form schema
@@ -45,9 +49,55 @@ const CreatePage = () => {
                 <h1 className="text-2xl">
                     Name your course
                 </h1>
-                <p>
+                <p className="text-sm text-slate-600">
                     what would you like to name your course
                 </p>
+                <Form {...form}>
+                    <form 
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8 mt-8"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="title"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Course Title
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'Advanced web development' "
+                                            {...field}
+                                        />
+
+                                    </FormControl>
+                                    <FormDescription>
+                                        What will you teach in this course
+                                    </FormDescription>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <Link href="/">
+                                <Button
+                                    type="button"
+                                    variant="ghost"
+                                >
+                                    Cancel
+                                </Button>
+                            </Link>
+                            <Button
+                                type="submit"
+                            >
+                                Continue
+                            </Button>
+                        </div>
+                    </form>
+
+                </Form>
             </div>
 
         </div>
