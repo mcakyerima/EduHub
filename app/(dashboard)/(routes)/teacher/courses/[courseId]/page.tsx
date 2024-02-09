e1e336bfaaea1877e5210c7be4d9e20a55f11d13
mcakyerima <mcakyerima@gmail.com>
2024-02-09 01:17:11 +0100

    updated remote origin

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,1 +1,11 @@
-const CourIdPage
+const CourseIdPage = ({
+    params
+}: { params: {courseId: string }}) {
+    return (
+        <div>
+            This is a course id
+        </div>
+    )
+}
+
+export default CourseIdPage;
