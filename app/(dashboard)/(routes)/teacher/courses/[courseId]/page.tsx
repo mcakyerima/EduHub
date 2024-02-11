012f24346b94222065cdacf9c342bed3f9573810
mcakyerima <mcakyerima@gmail.com>
2024-02-11 23:30:23 +0100

    completed course page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,5 @@
-import db from "@/lib/db";
-import { auth } form "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const CourseIdPage = async ({
@@ -37,14 +37,27 @@ const CourseIdPage = async ({
     const completedFields = requiredFields.filter(Boolean).length;
 
     // calculate completed fields from total fields
-    const completionText = `(${completedFields} / ${totalFields})`;
+    const completionText = `(${completedFields}/${totalFields})`;
     return (
         <div className="p-6">
             <div className="flex items-center justify-between">
-                <div>
-
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Course Setup
+                    </h1>
+                    <span className="text-sm text-slate-700">
+                        Complete all fields {completionText}
+                    </span>
                 </div>
             </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div className="flex items-center gap-x-2">
+                    <h2 className="text-xl">
+                        Customize your course
+                    </h2>
+                </div>
+            </div>
+
         </div>
     )
 }
