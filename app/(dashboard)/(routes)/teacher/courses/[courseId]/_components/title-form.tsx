924ca3c5b11f68ddc4d7e9f537969c7389a77235
mcakyerima <mcakyerima@gmail.com>
2024-02-14 23:59:10 +0100

    form_schema

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,5 +1,27 @@
 "use-client"
 
+import { z } from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+// validate form with zod schema validation
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "Title is required",
+    }),
+});
 interface TitleFormProps {
     initialData: {
         title: string;
@@ -11,6 +33,11 @@ export const TitleForm = ({
     initialData,
     courseId,
 }: TitleFormProps ) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    });
+    
     return ( 
         <div>
             Title Form
